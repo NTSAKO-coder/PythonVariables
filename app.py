c5a2021cf4e49b3afc1446b6779a06f5bac45d76
Ntsako-Coder <janelntsako@gmail.com>
2025-02-05 14:58:37 +0200

    variables added

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ quantity = 10
 print(f"He is ", age ,"years old")
 print(f"There are ",players , "players in their team")
 print(f"She's buying",quantity, "items")
+
+
+total_price = 6.82
+
+print(f"The item will cost ", total_price, "in rands")
+
+name = "Joy"
+surname = "Magagu"
+email = "joy@gmail.com"
+
+print(f"Hy ",name)
+print("Is your surname ",surname,"?")
+
+
+online = True
+
+print(f"Are you online?: {online}")
